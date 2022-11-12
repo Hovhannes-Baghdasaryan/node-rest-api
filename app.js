34fc037de9ac87543a7dac0cdd7b35afee1bcc07
Hovhannes <hovhannes.baghdasaryan.03@gmail.com>
2022-11-13 01:45:17 +0400

    commented app only kept simple endpoint

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ import mongoose from "mongoose";
 import postsRouter from "./routes/posts.js"
 import bodyParser from "body-parser";
 import cors from "cors";
-
-(async () => {
-    try {
-        mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true})
-        console.log("connected successfully")
-    } catch (error) {
-        console.log('connection failed', error)
-    }
-})()
+//
+// (async () => {
+//     try {
+//         mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true})
+//         console.log("connected successfully")
+//     } catch (error) {
+//         console.log('connection failed', error)
+//     }
+// })()
 
 const app = express()
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/posts', postsRouter)
+//
+// app.use(cors())
+// app.use(bodyParser.json())
+// app.use('/posts', postsRouter)
 
 // ROUTES
 app.get('/', (req, res) => {
